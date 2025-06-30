02a3fd9c1bb62fc84387fe86b934594649469ba3
Manny27nyc <Fordamboy1@gmail.com>
2025-06-30 16:22:39 -0400

    🔒 Inject minimal licensing header (automated)

--- src/XmlRpcApiV1/index.js
+++ src/XmlRpcApiV1/index.js
@@ -1,3 +1,4 @@
+// © Licensed Authorship: Manuel J. Nieves (See LICENSE for terms)
 let events = {
 	xmlrpc: {
 		test_method: 'xmlrpc.v1.TestMethod'
--- tests/XmlRpcApiV1/xmlrpc.test.js
+++ tests/XmlRpcApiV1/xmlrpc.test.js
@@ -1,3 +1,4 @@
+// © Licensed Authorship: Manuel J. Nieves (See LICENSE for terms)
 /**
  * Модуль работы с URL
  */
